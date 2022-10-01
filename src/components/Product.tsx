7c5b9953cf221df1f2481c65c8c2bce0ff8b9a08
b0nanze <iumkhaev@gmail.com>
2022-10-01 18:55:48 +0300

    start

---
+++ src/components/Product.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import {IProduct} from "../models";
+
+interface ProductProps {
+    product: IProduct
+}
+
+function Product( {product} : ProductProps) {
+    const [details, setDetails] = useState(false);
+
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+    const btnClasses = ['py-2 px-4 border', btnBgClassName]
+
+    return (
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img className="w-1/6" src={ product.image } alt={product.title}/>
+            <p>{product.title}</p>
+            <p className="font-bold">{product.price}$</p>
+            <button
+                className={btnClasses.join(' ')}
+                onClick={() => setDetails(prev => !prev)}
+            >
+                {details ? 'Hide details' : 'Show details'}
+            </button>
+            {
+                details &&
+              <div>
+                <p>{ product.description }</p>
+                <p> Rate: <span className="font-bold">{ product?.rating?.rate }</span></p>
+              </div>
+            }
+        </div>
+    );
+}
+
+export default Product;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutPage() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem excepturi incidunt laudantium officia officiis optio sed! Culpa earum facilis laboriosam, magnam possimus voluptatem? Commodi cum dolor eius enim fuga hic in, neque nostrum numquam provident recusandae sit totam voluptates. At dolor esse ipsam minima, optio placeat quibusdam velit vero? Odit.
+        </div>
+    );
+}
+
+export default AboutPage;
